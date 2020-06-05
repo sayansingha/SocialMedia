d58b454b0163af1285dbc02062057d62a2baddd7
Sayan Singha <sayansingha@Sayans-MacBook-Pro.local>
2020-06-06 03:55:26 +0530

    Profile - Profile thumbnails - Profile data

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,8 +1,10 @@
 import React,{useState,useEffect} from 'react';
-
+import {Link,useHistory} from 'react-router-dom'
+import M from 'materialize-css'
 const Home = () =>{
-
+    const history = useHistory();
     const [posts,setPosts] = useState([]);
+    const [comment, setComment] = useState("");
 
     useEffect(()=>{
         fetch("/allpost",{
@@ -13,7 +15,42 @@ const Home = () =>{
             setPosts(data.posts);
         }).catch(err=>console.log(err));
      },[])
-     
+
+     const PostComment=(postId)=>{
+        const item = JSON.parse(localStorage.getItem("user"));
+
+        const obj = {
+            comment: comment,
+            userName: item.name,
+            userId: item._id,
+            postId: postId
+        };
+        fetch("/comment",{
+            method:"post",
+            headers:{
+                "Content-Type":"application/json",
+                // "Authorization":"Bearer "+localStorage.getItem("jwt")
+            },
+            body: JSON.stringify({
+                comment: comment,
+                userName: item.name,
+                userId: item._id,
+                postId: postId
+            })
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            if(data.error){
+                M.toast({html: data.error})
+            }
+            else{
+                M.toast({html:data.message})
+                history.push('/');
+
+            }
+        }).catch(err=>console.log(err));
+    }
+
     return (
         <div className="home">
             {posts.map(post => {
@@ -23,10 +60,33 @@ const Home = () =>{
                     <div className="card-image">
                         <img src={post.photo} />
                     </div>
+                    <h6>
+                        Comments:
+                    </h6>
+                    {post.comments.map(comment => {
+                        return (<p>
+                            <strong>
+                                {comment.userName}
+                            </strong>
+                            <br />
+                            {comment.comment}
+                        </p>)
+                    })}
                     <div className="card-content">
                         <h5>{post.title}</h5>
                         <p>{post.body}</p>
-                        <input type="text" placeholder="add a comment"/>
+
+                        <input type="text" placeholder="add a comment"
+                            value={comment}
+                            onChange={(e)=>{
+                                setComment(e.target.value);
+                            }}
+                        />
+                        <button className="btn waves-effect waves-light" type="submit" name="action"
+                        onClick={PostComment.bind(this, post._id)}
+                        >
+                            Submit
+                        </button>
                     </div>
                 </div>)
             })}
--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -1,6 +1,34 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 
 const Profile = () =>{
+    const [posts, setPosts] = useState([]);
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    useEffect(()=>{
+         fetch("/mypost",{
+             method:"get",
+             headers:{
+                 "Content-Type":"application/json",
+                 "Authorization":"Bearer "+localStorage.getItem("jwt")
+             },
+         }).then(res=>res.json())
+         .then(data=>{
+            console.log(data.mypost);
+            setPosts(data.mypost);
+            // if(data.error){
+            //    M.toast({html: data.error,classes:"#c62828 red darken-3"})
+            // }
+            // else{
+            //     M.toast({html:"Created post Successfully",classes:"#43a047 green darken-1"})
+            //     history.push('/')
+            // }
+         }).catch(err=>{
+             console.log(err)
+         })
+     
+     },[])
+
+
     return (
         <div style={{maxWidth:"550px", margin:"0px auto"}}>
             <div style={{
@@ -9,15 +37,18 @@ const Profile = () =>{
                 margin: "18px 0px",
                 borderBottom:"1px solid grey"
             }}>
-                <div>
-                    <h4>Sayan Singha</h4>
-                    <div style={{display:"flex", justifyContent: "space-between", width:"108%"}}>
-                        <h6>40 posts</h6>
-                        <h6>40 followers</h6>
-                        <h6>40 following</h6>
-                    </div>
+            <div>
+                <h4>{user.name}</h4>
+                <div style={{display:"flex", justifyContent: "space-between", width:"108%"}}>
+                    <h6>{posts.length} posts</h6>
                 </div>
             </div>
+            </div>
+            {posts.map(post=>{
+                return(
+                    <img className="item" src={post.photo}/>
+                )
+            })}
         </div>
 
 
