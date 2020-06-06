0e243aab4100eeecade1b4d97c47e7615a1bbcd2
Sayan Singha <sayansingha@Sayans-MacBook-Pro.local>
2020-06-06 05:40:23 +0530

    added build files

---
+++ client/build/precache-manifest.55f3dad9fca0f4826ef015b0c5b0ec3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62787a3abf1ac6fbc71b7aa41466342",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abc7e314d483cddb9693",
+    "url": "/static/css/main.331ce794.chunk.css"
+  },
+  {
+    "revision": "e70c53dbc9d1befff310",
+    "url": "/static/js/2.e290d0e8.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.e290d0e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abc7e314d483cddb9693",
+    "url": "/static/js/main.b3cf7dd4.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
