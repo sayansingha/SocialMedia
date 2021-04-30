f0f8e5f1bfec167a1497fee867bb263976939516
sayansingha <sayansinghargj@gmail.com>
2021-04-30 21:06:20 +0530

    updated mongodb credentials

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 module.exports={
-    MONGOURI:"mongodb+srv://Sayan:fido_home@cluster0-n0bip.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    MONGOURI: "mongodb+srv://sayan:e2W8en3XPnzEukUo@cluster0.qiwo3.mongodb.net/socialmedia?retryWrites=true&w=majority",
     JWT_SECRET: "sayan"
 }
 
